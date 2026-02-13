3a70ccda812405dc46cc8ce68b27d197734ff96a
mdyahyaa0104 <mdyahyaa0104@gmail.com>
2026-02-13 13:56:31 -0600

    Stated and Finished Song.java and started working on Playlist.java

--- U4BLab/src/Playlist.java
+++ U4BLab/src/Playlist.java
@@ -1,2 +1,18 @@
+import java.util.ArrayList;
+
 public class Playlist {
+    private ArrayList<Song> songs;
+
+
+    public Playlist(ArrayList<Song> songs){
+        this.songs = songs;
+    }
+
+    public void sortArtist(String selection){
+
+    }
+    public void sortReleaseYear(){
+
+    }
+
 }
--- U4BLab/src/Song.java
+++ U4BLab/src/Song.java
@@ -1,2 +1,37 @@
 public class Song {
+    private String title, artist, album, genre;
+    private int durationSeconds, releaseYear;
+
+    public Song(String title, String artist, String album, int durationSeconds, int releaseYear, String genre){
+        this.title = title;
+        this.artist = artist;
+        this.album = album;
+        this.durationSeconds = durationSeconds;
+        this.releaseYear = releaseYear;
+        this.genre = genre;
+    }
+
+    public String toString(){
+        return (String.format("%-25s %-20s %-15s %-10d %-12.2f %-15s", title, artist, album, genre, durationSeconds, releaseYear));
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public String getArtist() {
+        return artist;
+    }
+    public String getAlbum() {
+        return album;
+    }
+    public int getDurationSeconds() {
+        return durationSeconds;
+    }
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+    public String getGenre() {
+        return genre;
+    }
+
 }
